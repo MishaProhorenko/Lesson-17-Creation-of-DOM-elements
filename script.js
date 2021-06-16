9f29d7032b4f08ac1e807c319a04919145f8220c
ZPT\Lnk2 <72851896+MishaProhorenko@users.noreply.github.com>
2021-06-16 15:17:47 +0300

    2 task

--- script.js
+++ script.js
@@ -28,6 +28,153 @@
 
 //#2
 
+const arrayOfPersons = [
+	{
+		name: 'Ferdinant',
+		surname: 'Foch',
+		age: 54,
+		nationality: 'French',
+		skill: 'Military management',
+		zodiacSign: undefined,
+		height: 173,
+		isAlive: false
+	},
+	{
+		name: 'Jackson',
+		surname: 'Bricks',
+		age: 20,
+		nationality: 'USA',
+		skill: 'farmer',
+		height: 187,
+		specialTrait: 'mechanical arms',
+		isFictionalCharacter: true
+	},
+	{
+		name: 'Hideo',
+		surname: 'Kozima',
+		age: 56,
+		nationality: 'USA',
+		skill: 'game developer',
+		height: 173,
+		specialTrait: 'genius',
+		actualGames: ['Death Stranding', 'Metal Gear'],
+		isAlive: true,
+	},
+	{
+		name: 'Kim',
+		surname: 'Kardashian',
+		age: 39,
+		nationality: 'USA',
+		skill: 'model',
+		height: 159,
+		zodiacSign: 'scorpio'
+	},
+	{
+		name: 'Mia',
+		surname: 'Khalifa',
+		age: 27,
+		nationality: 'Lebanon',
+		skill: 'actress',
+		height: 157,
+		zodiacSign: 'saggitarius',
+		specialTrait: 'very exspressionable',
+		isAlive: true,
+		isFictionalCharacter: false
+	},
+	{
+		name: 'Herman',
+		surname: 'Hesse',
+		age: 85,
+		nationality: 'Germany',
+		skill: 'writer',
+		zodiacSign: undefined,
+		isAlive: false,
+		isFictionalCharacter: false,
+		bibliography: ['Der Steppen Wolf', 'Das Glasperlenspiel',]
+	},
+	{
+		name: 'Fedor',
+		surname: 'Dostoevsky',
+		age: 60,
+		nationality: 'Russia',
+		skill: 'writer',
+		isAlive: false,
+		isFictionalCharacter: false,
+		bibliography: ['The demons', 'The Crime and Punishment', 'The Idiot']
+	},
+	{
+		name: 'Doomguy',
+		surname: undefined,
+		age: undefined,
+		nationality: undefined,
+		skill: 'demon slayer',
+		zodiacSign: undefined,
+		isFictionalCharacter: true,
+		specialTrait: 'Angry a bit',
+	},
+	{
+		name: 'Sonic',
+		surname: 'The Hedgehog',
+		age: 13,
+		nationality: undefined,
+		zodiacSign: undefined,
+		isFictionalCharacter: true,
+		specialTrait: 'Fast as f#@k',
+	},
+	{
+		name: 'Bruce',
+		surname: 'Wayne',
+		age: 12,
+		isFictionalCharacter: true
+	},
+	{
+		name: 'Ella',
+		surname: 'Fitzgerald',
+		age: 79,
+		isFrictionalCharacter: false,
+		isAlive: false,
+		specialTrait: 'Amazing voice',
+		signatureSongs: ['Cry Me a river', 'Hello Dolly', 'Summertime', 'In a sentimental mood']
+	}
+];
+
+
+// let container = document.createElement('div');
+// container.className = 'container';
+
+// arrayOfPersons.forEach(item => {	
+// 	let userCard = document.createElement('div');
+// 	userCard.className = 'userCard';
+// 	let userName = document.createElement('div');
+// 	let userSurName = document.createElement('div');
+// 	let userAge = document.createElement('div');
+// 	let userNationality = document.createElement('div');
+// 	document.body.append(container);
+// 	container.append(userCard);
+// 	userCard.append(userName, userSurName, userAge, userNationality);
+// 	userName.innerText = `Name: ${item.name}`;
+// 	userSurName.innerText = `Surname: ${item.surname}`;
+// 	userAge.innerText = `Age: ${item.age}`;
+// 	userNationality.innerText = `Nationality: ${item.nationality}`;	
+// });
+
+
+
+
+//=========================================================================
+//#3
+
+
+let wrapper = document.createElement('form');
+document.body.append(wrapper);
+let userNameInput = document.createElement('input');
+let emailInput = document.createElement('input');
+let passwordInput = document.createElement('input');
+let checkboxInput = document.createElement('input');
+let textArea = document.createElement('textarea');
+let radioBtnPaid = document.createElement('input');
+let radioBtnFree = document.createElement('input');
+let btn = document.createElement('button');
 
 
 
