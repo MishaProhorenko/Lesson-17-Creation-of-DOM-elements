df0445f9fceb0d26f38ff5f014742baffeafac62
ZPT\Lnk2 <72851896+MishaProhorenko@users.noreply.github.com>
2021-06-16 14:08:47 +0300

    1 task finish

---
+++ script.js
@@ -0,0 +1,34 @@
+'use strict';
+
+//===================================================================
+//#1
+
+// let findTagsOfElem = (id, tag) => {
+// 	let findId = document.getElementById(id);
+// 	let arrWhithTags = [];
+// 	if (findId) {
+// 		for (let tags of findId.children) {
+// 			if (tags.localName === tag) {
+// 				arrWhithTags.push(tags);
+// 			}
+// 		}
+// 	} else {
+// 		for (let tags of document.body.children) {
+// 			if (tags.localName === tag) {
+// 				arrWhithTags.push(tags);
+// 			}
+// 		}
+// 	}
+// 	return arrWhithTags.length;
+// };
+
+// console.log(findTagsOfElem('div', 'div'));
+
+//=========================================================================
+
+//#2
+
+
+
+
+//=========================================================================
